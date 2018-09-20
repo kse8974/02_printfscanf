936476799a2b0e31a2f9c55bb549eb65337eb562
kse8974 <kse8974@naver.com>
2018-09-20 16:50:33 +0900

    practice3

---
+++ practice 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+ int main(void) 
+{
+	char c;
+	
+	printf("enter a character : ");
+	scanf("%c", &c);
+	
+	printf("The next character of %c is %c \n", c, c+1);
+	return 0;
+}
+
+
+
